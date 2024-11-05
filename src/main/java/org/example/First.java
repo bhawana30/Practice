667f491e846a1ac2e3b0683cdf37c556f86e3348
bhawana30 <bhawanakhurana15@gmail.com>
2024-11-05 15:42:36 +0530

    Adding new loop in First.java

--- src/main/java/org/example/First.java
+++ src/main/java/org/example/First.java
@@ -4,6 +4,9 @@ public class First {
 
     public static void main(String args []){
         System.out.println("First Class");
+        for(int i =0; i<5; i++){
+            System.out.println("Value of i is :" + i);
+        }
     }
 
 }
