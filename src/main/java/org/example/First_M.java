9774414dbe3831b5fed2467ede5bb1a1f576fcc9
bhawana30 <bhawanakhurana15@gmail.com>
2024-11-05 16:31:38 +0530

    Adding new file

---
+++ src/main/java/org/example/First_M.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class First_M {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("In Master branch");
+    }
+}
