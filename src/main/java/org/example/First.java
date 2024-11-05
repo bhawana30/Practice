6d15114d22b8971a50e832ce0d40db5073014141
bhawana30 <bhawanakhurana15@gmail.com>
2024-11-05 16:39:49 +0530

    Checking for merge conflict

--- src/main/java/org/example/First.java
+++ src/main/java/org/example/First.java
@@ -6,6 +6,7 @@ public class First {
         System.out.println("First Class");
         for(int i =0; i<5; i++){
             System.out.println("Print value of i :" + i);
+            System.out.println("In main");
             i--;
         }
     }
