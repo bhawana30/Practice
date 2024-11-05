ee3f689fe82075d16792b0c50aad38c0fbbb7461
bhawana30 <bhawanakhurana15@gmail.com>
2024-11-05 15:44:43 +0530

    Changing value of i in loop in First.java

--- src/main/java/org/example/First.java
+++ src/main/java/org/example/First.java
@@ -5,7 +5,7 @@ public class First {
     public static void main(String args []){
         System.out.println("First Class");
         for(int i =0; i<5; i++){
-            System.out.println("Value of i is :" + i);
+            System.out.println("Print value of i :" + i);
             i--;
         }
     }
