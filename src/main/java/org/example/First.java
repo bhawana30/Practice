4d613b0069042cba46576ccda2fd3a426585b5cc
bhawana30 <bhawanakhurana15@gmail.com>
2024-11-05 16:50:51 +0530

    Changed value of i

--- src/main/java/org/example/First.java
+++ src/main/java/org/example/First.java
@@ -4,8 +4,9 @@ public class First {
 
     public static void main(String args []){
         System.out.println("First Class");
+        int y=0;
         for(int i =0; i<5; i++){
-            System.out.println("Value of i is :" + i);
+            System.out.println("Value of i is :" + y);
             System.out.println("In master");
             i--;
         }
